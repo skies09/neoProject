d1d08f0eeafeab89c78802c4169f709971f49272
Donna <skies09@hotmail.com>
2024-09-08 13:38:57 +0100

    add group and individual breed filter

--- breeds/urls.py
+++ breeds/urls.py
@@ -5,5 +5,7 @@ app_name = 'breeds'
 urlpatterns = [
     # breed views
     path('', views.breeds_list, name='breeds-list'),
-    path('groups/', views.breed_groups, name='breed-groups'),
+    path('groups', views.breed_groups, name='breed-groups'),
+    path('groups/<str:group>', views.group_breeds, name='breed-breeds'),
+    path('detail', views.breed_detail, name='breed-detail'),
 ]
--- breeds/views.py
+++ breeds/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from rest_framework import status
 from rest_framework.decorators import api_view
 from .models import Breed
 from rest_framework.views import APIView
@@ -16,3 +17,26 @@ def breeds_list(request):
 def breed_groups(request):
     groups =  Breed.objects.values_list('group', flat=True).distinct().order_by('group')
     return Response(groups)
+
+# Get the breeds in the group
+@api_view(['GET'])
+def group_breeds(request, group):
+    breeds = Breed.objects.filter(group=group)
+    serializer = BreedSerializer(breeds, many=True)
+    return Response(serializer.data)
+
+# Get individual breed view
+@api_view(['POST'])
+def breed_detail(request):
+    breed = request.data.get('breed')
+    
+    if not breed:
+        return Response({'error': 'No breed provided'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    try:
+        single_breed = Breed.objects.get(breed=breed)
+    except Breed.DoesNotExist:
+        return Response({'error': 'Breed not found'}, status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = BreedSerializer(single_breed)
+    return Response(serializer.data)
